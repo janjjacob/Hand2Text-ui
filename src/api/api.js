afb4e3ef35f9aa3aef83e36fdd4b7be067933424
janjjacob <janjjacob@gmail.com>
2025-04-08 22:51:30 -0400

    update backend URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://54.209.229.215:8000";
+const API_BASE_URL = "https://api/hand2text.com";
 
 export const recognizeFromURL = async (imageUrl) => {
   try {
